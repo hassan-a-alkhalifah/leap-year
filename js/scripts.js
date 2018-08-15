981d50d8d1c55da392880b5b6468ee026b5143f9
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-08-15 00:10:57 -0700

    add condition in leapYear function to check if input year is divisble by 400

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Business logic
 var leapYear = function(year) {
-  if ((year % 100 !=== 0) && (year % 4 === 0)) {
+  if ((year % 100 !== 0) && (year % 4 === 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;

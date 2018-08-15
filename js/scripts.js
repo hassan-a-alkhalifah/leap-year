d7d05e78a0a5f4d68d1944f5c25526b5f05ab292
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-08-15 00:06:15 -0700

    revise leapYear function conditional statements to check both if year is not divisible by 100 and is divisible by 4 on the same condition to dry code

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 // Business logic
 var leapYear = function(year) {
-  if(year % 100 === 0) {
-    return false;
-  } else if(year % 4 === 0) {
+  if ((year % 100 !=== 0) && (year % 4 === 0)) {
     return true;
   } else {
     return false;
